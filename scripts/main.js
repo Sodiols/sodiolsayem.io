55e60c5e19b6d63100595020d113979186db355e
Sodiol Sayem <134397635+Sodiols@users.noreply.github.com>
2024-03-29 00:37:07 +0600

    under construction

--- scripts/main.js
+++ scripts/main.js
@@ -4,16 +4,15 @@ new kursor ({
 })
 
 //! ext 
-const ucPopups = document.querySelectorAll('.uc-popup');
-const popup = document.querySelector('.popup');
 const okBtn = document.querySelector('.popup button');
 
-ucPopups.forEach(ucPopup => {
-    ucPopup.addEventListener("click", () => {
-        popup.classList.add("open-popup");
-    });
-});
 
-okBtn.addEventListener("click", () => {
-    popup.classList.remove("open-popup");
+okBtn.addEventListener('click', () => {
+    const email = prompt('Enter Your Email');
+
+    if (email !== null && email !== '') {
+        alert("We'll contact you via email");
+    } else {
+        alert("You've to put your email on the prompt")
+    }
 });
